90ad1b88b31a290f0bdad239d87a223250dc02ee
liaoli <277218962@qq.com>
2023-09-19 13:48:01 +0800

    顶部tabbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/login'
+import Layout from '@/views/layout'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    // path: '/login',
-    // component: Login
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/',
+    component: Layout
   }
 ]
 
---
+++ src/views/layout/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <van-tabbar v-model="active">
+  <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>
+  <van-tabbar-item icon="apps-o">分类页</van-tabbar-item>
+  <van-tabbar-item icon="shopping-cart-o">购物车</van-tabbar-item>
+  <van-tabbar-item icon="user-o">我的</van-tabbar-item>
+</van-tabbar>
+</template>
+
+<script>
+export default {
+  name: 'LayoutIndex',
+  data () {
+    return {
+      active: 0
+    }
+  }
+}
+</script>
+
+<style>
+</style>
