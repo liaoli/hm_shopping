d13cb8b1857fe29082379656ae3ef56f992ca02a
liaoli <277218962@qq.com>
2023-09-19 11:35:46 +0800

    综合项目练习

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <van-button type="primary"> 主要按钮</van-button>
+    <van-button type="info"> 信息按钮</van-button>
+    <van-button type="default"> 默认按钮</van-button>
+    <van-button type="warning"> 警告按钮</van-button>
+    <van-button type="danger"> 危险按钮</van-button>
     <router-view/>
   </div>
 </template>
