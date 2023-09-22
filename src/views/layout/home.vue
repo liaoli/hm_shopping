db478b048b85bbe45acd5167585b5998d1a09b8c
liaoli <277218962@qq.com>
2023-09-22 16:49:08 +0800

    首页

--- src/views/layout/home.vue
+++ src/views/layout/home.vue
@@ -14,23 +14,17 @@
 
     <!-- 轮播图 -->
     <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-      <van-swipe-item>
-        <img src="@/assets/banner1.jpg" alt="">
-      </van-swipe-item>
-      <van-swipe-item>
-        <img src="@/assets/banner2.jpg" alt="">
-      </van-swipe-item>
-      <van-swipe-item>
-        <img src="@/assets/banner3.jpg" alt="">
+      <van-swipe-item v-for="item in bannerList" :key="item.imgUrl">
+        <img :src="item.imgUrl" alt="">
       </van-swipe-item>
     </van-swipe>
 
     <!-- 导航 -->
     <van-grid column-num="5" icon-size="40">
       <van-grid-item
-        v-for="item in 10" :key="item"
-        icon="http://cba.itlike.com/public/uploads/10001/20230320/58a7c1f62df4cb1eb47fe83ff0e566e6.png"
-        text="新品首发"
+        v-for="item in navList" :key="item.imgUrl"
+        :icon="item.imgUrl"
+        :text="item.text"
         @click="$router.push('/category')"
       />
     </van-grid>
@@ -45,7 +39,7 @@
       <p class="guess-title">—— 猜你喜欢 ——</p>
 
       <div class="goods-list">
-        <GoodsItem v-for="item in 10" :key="item"></GoodsItem>
+        <GoodsItem v-for="item in proList" :key="item.imgUrl" :item="item"></GoodsItem>
       </div>
     </div>
   </div>
@@ -53,10 +47,29 @@
 
 <script>
 import GoodsItem from '@/components/GoodsItem.vue'
+import { getHomeData } from '@/api/home'
 export default {
   name: 'HomePage',
   components: {
     GoodsItem
+  },
+  data () {
+    return {
+      bannerList: [],
+      navList: [],
+      proList: []
+    }
+  },
+  async created () {
+    await this.getHomeData()
+  },
+  methods: {
+    async getHomeData () {
+      const { data: { pageData } } = await getHomeData()
+      this.bannerList = pageData.items[1].data
+      this.navList = pageData.items[3].data
+      this.proList = pageData.items[6].data
+    }
   }
 }
 </script>
